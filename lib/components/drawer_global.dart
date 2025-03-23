0f8bbf40858035e3d7a75c7dcbd01186c40b73f5
yasinowo <zyzypwrmhmdyasyn@gmail.com>
2025-03-23 13:49:58 +0330

    drawer complte

---
+++ lib/components/drawer_global.dart
@@ -0,0 +1,98 @@
+import 'package:chatapp_supabase/page/setting_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DrawerGlobal extends StatelessWidget {
+  const DrawerGlobal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.onPrimary,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              // logo
+              DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.message,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 40.sp,
+                  ), // Icon
+                ), // Center
+              ),
+              // home list tile
+              Padding(
+                padding: EdgeInsets.only(left: 20.w),
+                child: ListTile(
+                  title: Text(
+                    "H O M E",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary),
+                  ),
+                  leading: Icon(
+                    Icons.home,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 25.sp,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ), // ListTile
+              ), // Padding
+
+              // settings list tile
+              Padding(
+                padding: EdgeInsets.only(left: 20.w),
+                child: ListTile(
+                  title: Text(
+                    "S E T T I N G S",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary),
+                  ),
+                  leading: Icon(
+                    Icons.settings,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 25.sp,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SettingPage()));
+                  },
+                ), // ListTile
+              ), // Padding
+            ],
+          ),
+          // logout list tile
+          Padding(
+            padding: EdgeInsets.only(left: 20.w, bottom: 20.h),
+            child: ListTile(
+              title: Text(
+                "L O G O U T",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary),
+              ),
+              leading: Icon(
+                Icons.logout,
+                color: Theme.of(context).colorScheme.primary,
+                size: 25.sp,
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ), // ListTile
+          ), // Padding
+        ],
+      ), // Column
+    ); // Drawer
+  }
+}
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:chatapp_supabase/auth/auth_service.dart';
+import 'package:chatapp_supabase/components/drawer_global.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,15 +7,14 @@ class HomePage extends StatelessWidget {
 
   void logout(BuildContext context) {
     // get auth service
-    final _auth = AuthService();
-    _auth.signOut();
-    // navigate to login page
-    // Navigator.pushReplacementNamed(context, '/login');
+    final auth = AuthService();
+    auth.signOut();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const DrawerGlobal(),
       appBar: AppBar(
         title: const Text("Home"),
         actions: [
